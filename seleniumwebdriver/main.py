e4d67044a9424ed6c83603c4c96dc0a714e80ca7
awes0m <somsubhra.pandit@gmail.com>
2021-08-16 09:46:04 +0530

    code refactored

--- seleniumwebdriver/main.py
+++ seleniumwebdriver/main.py
@@ -6,11 +6,9 @@ driver=webdriver.Chrome(executable_path=chrome_driver_path)
 web=driver.get("https://www.python.org/")
 event_times=driver.find_elements_by_css_selector(".event-widget time")
 event_names=driver.find_elements_by_css_selector(".event-widget li a")
-event={}
-for _ in range(len(event_times)):
-    event[_]={
+event = {_: {
         "time":event_times[_].text,
         "name":event_names[_].text,
-    }
+    } for _ in range(len(event_times))}
 print(event)
 driver.close()
